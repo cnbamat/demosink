57405ad0f9723f67e8d078de519bc1d5f4e59ee3
Bamatembera <Christian.Bamatembera@cyber.gc.ca>
2021-01-14 12:59:14 -0500

    first commit

---
+++ src/main/java/com/cbamatembera/demosink/DemoSinkApplication.java
@@ -0,0 +1,13 @@
+package com.cbamatembera.demosink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSinkApplication.class, args);
+	}
+
+}
